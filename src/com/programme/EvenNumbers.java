3a5af20b93400d3d461e1ecf81adce72aae32311
MuthuMK231 <muthumkm1234@gmail.com>
2024-04-15 13:26:52 +0530

    added new class

--- src/com/programme/EvenNumbers.java
+++ src/com/programme/EvenNumbers.java
@@ -5,9 +5,9 @@ public class EvenNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			int i,n=10;
-		for(i=2;i<=10;i=i+2) {
-			System.out.println(i);
-		}
+		// for(i=2;i<=10;i=i+2) {
+	//		System.out.println(i);
+//		}
 			
 			
 			for(i=1;i<=10;i++) {
